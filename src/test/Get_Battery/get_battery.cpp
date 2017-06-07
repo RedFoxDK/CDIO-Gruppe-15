df3921aac0757afe2d2eb4657117469ac751d8f5
Andreas <andreasfyhn@hotmail.com>
2017-06-07 10:06:44 +0100

    Created working qr_reader node

--- src/test/Get_Battery/get_battery.cpp
+++ src/test/Get_Battery/get_battery.cpp
@@ -8,11 +8,20 @@ void chatterCallback(const ardrone_autonomy::Navdata::ConstPtr& msg) {
   ROS_INFO("Temperature: %d", msg->temp);
   ROS_INFO("pressure: %d", msg->pressure);
   ROS_INFO("Altitude: %d", msg->altd);
+  ROS_INFO("left/right tilt (rotX): %f", msg->rotX);
+  ROS_INFO("forward/backward tilt (rotY): %f", msg->rotY);
+  ROS_INFO("rotation (rotZ): %f", msg->rotZ);
+  ROS_INFO("linear velocity (vx): %f", msg->vx);
+  ROS_INFO("linear velocity (vy): %f", msg->vy);
+  ROS_INFO("linear velocity (vz): %f", msg->vz);
+  ROS_INFO("acceleration (ax): %f", msg->ax);
+  ROS_INFO("acceleration (ay): %f", msg->ay);
+  ROS_INFO("acceleration (az): %f", msg->az);
 }
 
 int main(int argc, char **argv) 
 {
-  ros::init(argc, argv, "Get_Battery");
+  ros::init(argc, argv, "drone_fly");
   
   ros::Subscriber battery_sub;
   std::string battery_channel;
